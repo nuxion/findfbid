f3c0aafc14e42f692d40c537470f9e39927d3b51
Xavier Petit <nuxion@gmail.com>
2016-10-31 00:22:37 -0300

    they have beautifoul

--- reqpage.py
+++ reqpage.py
@@ -1,10 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 'Mozilla/5.0'}
 payload = {'url':'https://www.facebook.com/fotoclubcordoba.oficial'}
 
-
-
 session = requests.Session()
 response=session.post("http://findmyfbid.com/", headers=headers, data=payload)
-print (response.text)
+soup=BeautifulSoup(response.text,"html.parser")
+print("here")
+#code = soup.find_all('code').getText()
+code = soup.code.getText()
+print(code)
+#print(response.text)
